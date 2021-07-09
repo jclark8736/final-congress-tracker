8fb09a65dc26d89bdd87569f479cdd83b2342b58
David-V-Rullo <d.v.rullo@gmail.com>
2021-07-09 16:01:54 -0400

    Integrating into new repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PageHead from "./Components/PageHead"
 import Navigation from './Components/Navigation';
+import LinkSideBar from './Components/LinkSideBar';
+import SearchSideBar from './Components/SearchSideBar';
+import MainPage from './Components/MainPage';
 
 
 function App() {
   return (
-    <div>
+    <div style={{
+      backgroundColor: 'navy'
+    }}>
     <PageHead />
     <Navigation />
     <Router>
-      
+    <MainPage />  
     </Router>
     </div>
   );
---
+++ client/src/components/LinkSideBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Header, Image, Grid } from "semantic-ui-react";
+const LinkSideBar = () => {
+  return (
+    <Container style={{
+        backgroundColor: 'maroon',
+        padding: '2em',
+        color:'white'
+    }}>
+      <Grid >
+        <Grid.Row columns={2}>
+          <Grid.Column >
+            <Header size='medium' style={{color: '#ffffff'}}><a style={{color:'white'}} href="https://live.house.gov/">
+              United States Congress Live Session
+            </a></Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              size="small"
+              src={
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Seal_of_the_United_States_House_of_Representatives.svg/240px-Seal_of_the_United_States_House_of_Representatives.svg.png"
+              }
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+          <Header size='medium'>
+          <a style={{color:'white'}} href="https://www.senate.gov/floor/index.htm">
+            United States Senate Live Session
+          </a>
+          </Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              size="small"
+              src={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/US-Senate-UnofficialAltSeal.svg/240px-US-Senate-UnofficialAltSeal.svg.png"}
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+          <Header size='medium'>
+          <a style={{color:'white'}} href="https://www.whitehouse.gov/about-the-white-house/our-government/the-executive-branch/">
+            The Executive Branch
+          </a>
+          </Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              size="small"
+              src={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Seal_of_the_President_of_the_United_States.svg/240px-Seal_of_the_President_of_the_United_States.svg.png"}
+            />
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
+    </Container>
+  );
+};
+
+export default LinkSideBar;
