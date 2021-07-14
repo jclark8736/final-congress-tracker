0d4142bc3595fca65eed01fa02e9759691d2831a
Patrick Braun <prbraun@protonmail.com>
2021-07-14 11:52:44 -0500

    Updated auth payload

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,8 +28,8 @@ module.exports = {
     // return the request object so it can be passed to the resolver as `context`
     return req;
   },
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
+  signToken: function ({ firstName, lastName, email, zip, password }) {
+    const payload = { firstName, lastName, email, zip, password };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
