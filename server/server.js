713b29c3c82b27dcc13e275e2843b62370ef65e7
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-07-15 12:06:34 -0400

    routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const routes = require("./controllers");
+const router = require('express').Router();
+
+//app.use(routes)?
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +22,9 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -25,6 +34,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
@@ -32,3 +43,7 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+
+app.use(require("./controllers/ext-routes.js"));
+
+
